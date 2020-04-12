69d68a5af1139c38956a7f6eb31bbf0f9bbcea5e
lemaiwo <wouterlemaire120@hotmail.com>
2020-04-12 21:39:02 +0200

    Update approuter-start.js

--- approuter-start.js
+++ approuter-start.js
@@ -1,4 +1,6 @@
 const approuter = require('@sap/approuter');
+var xssec = require('@sap/xssec');
+var xsenv = require('@sap/xsenv');
 const rp = require('request-promise');
 const parseString = require('xml2js').parseString;
 const jwtDecode = require('jwt-decode');
@@ -9,6 +11,23 @@ const sUaaCredentials = dest_service.credentials.clientid + ':' + dest_service.c
 const sDestinationName = 'IAS';
 const sEndpoint = "/service/users?mail=";
 var ar = approuter();
+
+function getUserInfo(token) {
+	return new Promise((resolve, reject) => {
+		xssec.createSecurityContext(token, xsenv.getServices({
+				uaa: {
+					tag: 'xsuaa'
+				}
+			}).uaa,
+			function (error, securityContext) {
+				if (error) {
+					console.log('Security Context creation failed');
+					return;
+				}
+				resolve(securityContext);
+			});
+	});
+};
 ar.beforeRequestHandler.use('/getuserinfo', function (req, res, next) {
 	if (!req.user) {
 		res.statusCode = 403;
@@ -16,18 +35,24 @@ ar.beforeRequestHandler.use('/getuserinfo', function (req, res, next) {
 	} else {
 		res.statusCode = 200;
 		var decodedJWTToken = jwtDecode(req.user.token.accessToken);
-		rp({
-			uri: uaa_service.credentials.url + '/oauth/token',
-			method: 'POST',
-			headers: {
-				'Authorization': 'Basic ' + Buffer.from(sUaaCredentials).toString('base64'),
-				'Content-type': 'application/x-www-form-urlencoded'
-			},
-			form: {
-				'client_id': dest_service.credentials.clientid,
-				'grant_type': 'client_credentials'
-			}
-		}).then(function (data) {
+		var securityContext;
+		getUserInfo(req.user.token.accessToken).then((securityContextResult) => {
+			securityContext = securityContextResult;
+			return securityContext;
+		}).then(() => {
+			return rp({
+				uri: uaa_service.credentials.url + '/oauth/token',
+				method: 'POST',
+				headers: {
+					'Authorization': 'Basic ' + Buffer.from(sUaaCredentials).toString('base64'),
+					'Content-type': 'application/x-www-form-urlencoded'
+				},
+				form: {
+					'client_id': dest_service.credentials.clientid,
+					'grant_type': 'client_credentials'
+				}
+			});
+		}).then((data) => {
 			const token = JSON.parse(data).access_token;
 			return rp({
 				uri: dest_service.credentials.uri + '/destination-configuration/v1/destinations/' + sDestinationName,
@@ -35,7 +60,7 @@ ar.beforeRequestHandler.use('/getuserinfo', function (req, res, next) {
 					'Authorization': 'Bearer ' + token
 				}
 			});
-		}).then(function (data) {
+		}).then((data) => {
 			const oDestination = JSON.parse(data);
 			const token = oDestination.authTokens[0];
 			return rp({
@@ -45,16 +70,17 @@ ar.beforeRequestHandler.use('/getuserinfo', function (req, res, next) {
 					'Authorization': `${token.type} ${token.value}`
 				}
 			});
-		}).then(function (xmldata) {
-			parseString(xmldata, function (err, IASResult) {
+		}).then((xmldata) => {
+			parseString(xmldata, (err, IASResult)=>{
 				res.end(JSON.stringify({
-					name:req.user.name,
+					name: req.user.name,
 					user: req.user,
+					securityContext: securityContext,
 					decodedJWTToken: decodedJWTToken,
 					iasData: IASResult
 				}));
 			});
-		}).catch(function (error) {
+		}).catch((error) => {
 			res.end("error: " + JSON.stringify(error));
 		});
 	}
